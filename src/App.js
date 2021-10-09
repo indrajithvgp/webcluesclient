8f3d75662dd3b4a8fe002bcdfebc804d3e4ea291
Indrajith V <indrajithvgp@yahoo.com>
2021-10-09 20:34:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {BrowserRouter,Redirect, Switch, Route} from 'react-router-dom'
 import CreateForm from './components/CreateForm';
 import Form from './components/Form';
-
+import Check from './components/Check.jsx'
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={"/"} component={CreateForm} />
-        <Route exact path={"/form/:id"} component={Form} />
+        <Route path={"/form/:id"} component={Form} />
+        <Route exact path={"/check"} component={Check} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/Check.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Check = () => {
+  return <div>hello</div>;
+};
+
+export default Check;
